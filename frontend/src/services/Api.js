a5b78b7efcb09ee56cc63c538eced9212358053d
Pacifique001 <tuyizerepacifique053@gmail.com>
2025-11-20 14:46:03 +0200

    new commit

--- frontend/src/services/Api.js
+++ frontend/src/services/Api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const base = process.env.REACT_APP_API_BASE || 'http://api.paygoat.local';
+const base = process.env.REACT_APP_API_BASE || 'http://api.paygoat.local:3000';
 
 const api = axios.create({
   baseURL: base,
